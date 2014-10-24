3b49baf0f8507753c803ba5533401013e3cafe09
roc@fewlaps.com <roc@fewlaps.com>
2014-10-24 22:25:14 +0200

    Added the graph library: https://github.com/PhilJay/MPAndroidChart

--- mobile/src/main/java/com/fewlaps/android/donaelpas/MainActivity.java
+++ mobile/src/main/java/com/fewlaps/android/donaelpas/MainActivity.java
@@ -2,13 +2,49 @@ package com.fewlaps.android.donaelpas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+    LineChart chart;
+    TextView todaySteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        todaySteps = (TextView) findViewById(R.id.todaySteps);
+        todaySteps.setText(getString(R.string.steps, 18294));
+
+        chart = (LineChart) findViewById(R.id.chart);
+
+        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
+        valsComp1.add(new Entry(12047, 0));
+        valsComp1.add(new Entry(9214, 1));
+        valsComp1.add(new Entry(15242, 2));
+        valsComp1.add(new Entry(18294, 3));
+
+        LineDataSet setComp1 = new LineDataSet(valsComp1, null);
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(setComp1);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.Q");
+        xVals.add("2.Q");
+        xVals.add("3.Q");
+        xVals.add("4.Q");
+
+        LineData data = new LineData(xVals, dataSets);
+
+        chart.setData(data);
     }
 }
