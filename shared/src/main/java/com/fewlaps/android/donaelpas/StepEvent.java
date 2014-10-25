9c6f2c9ee28f1bd608f090229463c6856db8fbbb
Esteve <esteveaguilera@gmail.com>
2014-10-25 05:06:41 +0200

    Shared stuf goes to shared module

--- shared/src/main/java/com/fewlaps/android/donaelpas/StepEvent.java
+++ shared/src/main/java/com/fewlaps/android/donaelpas/StepEvent.java
@@ -12,10 +12,10 @@ public class StepEvent {
     public StepEvent(long steps) {
         lastWeekSteps = new ArrayList(7);
         lastWeekSteps.add(steps); //today
-        lastWeekSteps.add(12472l); //yesterday
-        lastWeekSteps.add(2574l); //... guess it! :P
-        lastWeekSteps.add(4378l);
-        lastWeekSteps.add(24178l);
+        lastWeekSteps.add(8472l); //yesterday
+        lastWeekSteps.add(9574l); //... guess it! :P
+        lastWeekSteps.add(7378l);
+        lastWeekSteps.add(8178l);
         lastWeekSteps.add(9261l);
         lastWeekSteps.add(10242l);
     }
