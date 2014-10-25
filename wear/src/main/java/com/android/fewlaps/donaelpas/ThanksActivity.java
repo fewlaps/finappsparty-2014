66f65979551232ad6d430a633f6d4c75d5e1e7ba
Esteve <esteveaguilera@gmail.com>
2014-10-25 11:39:00 +0200

    Added confirmation activity when sharing on Facebook/Twitter

--- wear/src/main/java/com/android/fewlaps/donaelpas/ThanksActivity.java
+++ wear/src/main/java/com/android/fewlaps/donaelpas/ThanksActivity.java
@@ -1,9 +1,13 @@
 package com.android.fewlaps.donaelpas;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 
 import com.fewlaps.android.donaelpas.R;
 
@@ -20,9 +24,19 @@ public class ThanksActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
 
+                View.OnClickListener sharedListener = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(ThanksActivity.this, ConfirmationActivity.class);
+                        intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
+                        intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, getString(R.string.fet));
+                        startActivity(intent);
+                    }
+                };
+
+                stub.findViewById(R.id.shareFacebook).setOnClickListener(sharedListener);
+                stub.findViewById(R.id.shareTwitter).setOnClickListener(sharedListener);
             }
         });
-
     }
-
 }
